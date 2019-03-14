dede42d3dd096fdd6a7cdcb8eae19e77a1292487
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-03-13 17:07:17 -0700

    style.css

--- config/orm.js
+++ config/orm.js
@@ -5,9 +5,7 @@ var orm = {
         var queryString = "SELECT * FROM ?? ;";
 
         connection.query(queryString, [table], function(err, result) {
-            if (err) {
-                throw err;
-            }
+            if (err) throw err;
             cb(result);
         });
     },
@@ -15,20 +13,16 @@ var orm = {
     insertOne: function(table, column_name, colVal, cb) {
         var queryString = "INSERT INTO ?? (??, devoured) VALUES (??, false) ;";
 
-        connection.query(queryString, [table, column_name, colVal], function (err, result) {
-            if (err) {
-                throw err;
-            }
+        connection.query(queryString, [table, column_name, colVal], function(err, result) {
+            if (err) throw err;
             cb(result);
         });
     },
 
     updateOne: function(table, column_name, colId, cb) {
         var queryString = "UPDATE ?? SET ?? = true WHERE id = ? ;";
-        connection.query(queryString, [table, column_name, colId], function (err, result) {
-            if (err) {
-                throw err;
-            }
+        connection.query(queryString, [table, column_name, colId], function(err, result) {
+            if (err) throw err;
             cb(result);
         });
     }
