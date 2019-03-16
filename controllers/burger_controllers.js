78aca8ec33cacb43ddf3554bd29c952d8baa9c1b
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-03-16 15:17:43 -0700

    heroku

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,6 +1,7 @@
-var burger = require("./../models/burger.js");
 var express = require("express");
 var router = express.Router();
+var burger = require("./../models/burger.js");
+
 
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
