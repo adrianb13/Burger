b1efe3b09b2bad40142e73c896045b9290ca90e6
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-03-16 14:53:40 -0700

    heroku

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ var orm = {
         var queryString = "SELECT * FROM ?? ";
 
         connection.query(queryString, [table], function(err, result) {
-            if (err) throw err;
+            if (err) {throw err};
             cb(result);
         });
     },
@@ -14,7 +14,7 @@ var orm = {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
 
         connection.query(queryString, [table, column_name, colVal], function(err, result) {
-            if (err) throw err;
+            if (err) {throw err};
             cb(result);
         });
     },
@@ -22,7 +22,7 @@ var orm = {
     updateOne: function(table, column_name, colId, cb) {
         var queryString = "UPDATE ?? SET ?? = true WHERE id = ?";
         connection.query(queryString, [table, column_name, colId], function(err, result) {
-            if (err) throw err;
+            if (err) {throw err};
             cb(result);
         });
     }
